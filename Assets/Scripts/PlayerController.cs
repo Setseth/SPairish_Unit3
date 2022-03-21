2e15f1f0b74bb0595564b4242c53389d8c980071
Setseth <Sethvpairish@gmail.com>
2022-03-20 19:22:58 -0700

    Unit3.Started

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
+    public float jumpforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if(spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+    }
+}
