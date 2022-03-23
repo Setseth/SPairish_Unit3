b249cdda3ec419bde7f48352f733a142c3764ed6
Setseth <Sethvpairish@gmail.com>
2022-03-23 10:36:28 -0700

    FinishedLessonA.Unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rbPlayer;
     public float gravityModifier;
     public float jumpforce;
+    public bool onGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +20,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if(spaceDown)
+        if (spaceDown && onGround)
         {
+            // jump code
             rbPlayer.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            onGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
     }
 }
